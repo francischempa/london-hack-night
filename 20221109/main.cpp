bab1982827b0d4f61566f84e918fdb928cd70237
Francis Chempa <dev.chempa@gmail.com>
2022-11-09 17:26:21 +0000

    test

--- 20221109/main.cpp
+++ 20221109/main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(int argc, char** argv)
-{	
+{
+	int a = 0;
+    int b = 2;
+    int sum = a + b;
+    cout << "Sum = " << sum << std::endl;
+    {
+        int c = 4;
+        int d = 434;
+        int e = c+d;
+        cout << "Sum = " << e << std::endl;
+    }
 	cout << "Hello World!!!\n";
 	return 0;
 }
